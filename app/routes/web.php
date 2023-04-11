ca821ce5f0969c97adf939d2a2a47f9d021c487e
Fronidze <Fronidze@me.com>
2023-04-11 10:50:27 +0300

    fixed teams admin pages

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('admin')->name('panel.')->group(function () {
+Route::prefix('admin-africa')->name('panel.')->group(function () {
     Route::get('/', [PanelController::class, 'index'])->name('index');
 
     Route::prefix('gallery')->name('gallery.')->group(function () {
